bc97bfa3c4cfe92a079322a4e1557bcea754912e
Ilya Baldin <ibaldin@renci.org>
2019-07-10 16:34:38 -0400

    setup.py for publishing to PyPi. Not tested yet to run as a downloadable module

--- wp_dso_publish/setup.py
+++ wp_dso_publish/setup.py
@@ -1,12 +1,27 @@
-from setuptools import setup
+#!/usr/bin/env python
 
-setup(
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name='wp_dso_publish',
-    version='0.1',
-    packages=[''],
-    url='',
+    version='0.12',
+    packages=setuptools.find_packages(),
+    url='https://github.com/RENCI-NRIG/impact-utils/tree/master/wp_dso_publish',
     license='MIT',
+    long_description=long_description,
     author='ibaldin',
     author_email='ibaldin@renci.org',
-    description='Utility to push SAFE policies for a specific dataset.'
+    description='Utility to push SAFE policies for a specific dataset.',
+    classifiers=[
+	"Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+      'pyforms==4.0.3',
+      'pycryptodome==3.8.2',
+    ],
 )
